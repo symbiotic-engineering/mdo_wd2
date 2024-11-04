4205268f8d979f1a3155b45e8bd239f63d14c8a8
degoeden <degoeden8@gmail.com>
2024-11-04 09:58:21 -0500

    membrane parameters are vars

--- src/run_basic.m
+++ src/run_basic.m
@@ -1,7 +1,11 @@
+clear;clc;close all;
 %% Define Parameters
 %  Piston
-piston_area = 0.26; % Piston Area
-piston_stroke = 12; % Piston Stroke Length
+piston_area = 0.26;     % [m^2]     Piston Area
+piston_stroke = 12;     % [m]       Piston Stroke Length
 
+%  Membrane
+mem_resist = 60.23;     % [MPa*s/m^3]   Membrane Hydraulic Resistance
+mem_pressure_min = 30;  % [bar]         Minimum Required Pressure Across Membrane
 %% Run WEC-Sim
 wecSim
