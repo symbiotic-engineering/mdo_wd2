e1fcea4d68d1b39c8a63bca664760aa5a119f230
degoeden <degoeden8@gmail.com>
2024-11-25 12:25:45 -0500

    when swtiched to hydraulics and solver/derivatives are changed, WEC-Sim Apps matches me

--- test/testYuJenne.m
+++ test/testYuJenne.m
@@ -1,6 +1,7 @@
 clear;clc;close all;
 %% Select Model
 model = '../src/YuJenne';
+model = '../src/wecSimApps_converted'
 
 %% Define Parameters
 % Drivetrain
@@ -27,4 +28,4 @@ wave_type = 'YuJenne';
 %% Run WEC-Sim
 wecSim
 
-save('../data/validation/myVersion.mat','output','simout1')
+save('../data/validation/wecSimApps_converted.mat','output','simout1')
