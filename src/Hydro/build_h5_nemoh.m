8cbda6a20c34d69021b6a1f5df6577f6ea4037b2
degoeden <degoeden8@gmail.com>
2025-01-06 15:57:05 -0500

    nemoh integration in other repo, will submod soon

--- src/Hydro/build_h5_nemoh.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = build_h5_nemoh(inputArg1,inputArg2)
-%BUILD_H5_NEMOH Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
