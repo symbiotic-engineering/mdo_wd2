fb0d0e8dcc8bd342d970a18e3fb5c368652e7892
degoeden <degoeden8@gmail.com>
2025-02-12 13:20:11 -0500

    params made into floats

--- src/params.py
+++ src/params.py
@@ -4,11 +4,11 @@ PARAMS = {}
 
 #   Hydro Params
 PARAMS["g"] = 9.81    # acceleration due to gravity
-PARAMS["rho"] = 1025  # density of seawater
+PARAMS["rho"] = 1025. # density of seawater
 PARAMS["body_name"] = 'Flap'  # name of the body
 PARAMS["water_depth"] = 12. # depth of the water
-PARAMS["forward_speed"] = 0  # forward speed of the body
-PARAMS["wave_direction"] = np.array(0)  # direction of the waves
+PARAMS["forward_speed"] = 0. # forward speed of the body
+PARAMS["wave_direction"] = np.array(0.) # direction of the waves
 PARAMS["omega"] = np.linspace(0.2,3,10) # wave frequencies
 PARAMS["dof"] = 'Pitch'  # degree of freedom
 
