a36f8d859e1d3376c19fcd93f4b57f4a6d0e7edc
degoeden <degoeden8@gmail.com>
2025-03-31 15:31:29 -0400

    econ test updates

--- test/testecon.py
+++ test/testecon.py
@@ -5,6 +5,8 @@ sys.path.append(parent_folder)
 import unittest
 import src.econ.econ as econ
 import src.econ.PTOecon as PTOecon
+import src.econ.ROecon as ROecon
+import src.econ.WECecon as WECecon
 import numpy as np
 from src.params import PARAMS
 
@@ -33,29 +35,72 @@ class TestEcon(unittest.TestCase):
 
     def test_accum4(self):
         # Test accumulator cost
+        from src.privateparams import PRIVATEPARAMS
         accum_vol = 4.0
         cost = PTOecon.accum_cost(accum_vol)
-        expected_cost = PARAMS["accum_cost_15G"]*70 + PARAMS["accum_cost_5G"]*1 + PARAMS["accum_cost_2.5G"]*1
+        expected_cost = PRIVATEPARAMS["accum_cost_15G"]*70 + PRIVATEPARAMS["accum_cost_5G"]*1 + PRIVATEPARAMS["accum_cost_2.5G"]*1
         print(f"4 m^3 accumulator cost : ${cost}")
-        np.testing.assert_allclose(cost,expected_cost)
+        np.testing.assert_allclose(cost,expected_cost,rtol=0.01)
     
-    def test_accum004g(self):
+    def test_accum004(self):
         # Test accumulator cost
+        from src.privateparams import PRIVATEPARAMS
         accum_vol = 0.04
         cost = PTOecon.accum_cost(accum_vol)
-        expected_cost = PARAMS["accum_cost_10G"] + PARAMS["accum_cost_2.5G"]
+        expected_cost = PRIVATEPARAMS["accum_cost_10G"] + PRIVATEPARAMS["accum_cost_2.5G"]
         print(f"0.04 m^3 accumulator cost : ${cost}")
-        np.testing.assert_allclose(cost,expected_cost)
+        np.testing.assert_allclose(cost,expected_cost,rtol=0.01)
 
     def test_link(self):
         # Test link cost
-        l1 = 2
-        l2 = 5
+        linches = 96
+        dinches = 24
+        lmeters = linches*0.0254
+        dmeters = dinches*0.0254
+        area = np.pi*(dmeters/2)**2
+        l1 = 0
+        l2 = lmeters
         l3 = 0
-        force = 63e6*0.26
+        force = 6e6*area
         cost = PTOecon.link_cost(l1,l2,l3,force)
         print(f"link cost : ${cost}")
         np.testing.assert_allclose(cost,cost)
+
+    def test_RO_capex(self):
+        # Test RO opex
+        feedcap = 3100/0.45
+        capacity = 3100
+        pipelength = 500
+        feedTDS = 35000
+        cost = ROecon.CAPEX(feedcap,capacity,pipelength,feedTDS)
+        print(f"RO opex : ${cost}")
+        np.testing.assert_allclose(cost,9159667,rtol=0.01)
+
+    def test_RO_opex(self):
+        # Test RO opex
+        feedflow = 3100/0.45*0.49
+        permflow = 3100*0.49
+        pipelength = 500
+        feedTDS = 35000
+        cost = ROecon.OPEX(feedflow,permflow,pipelength,feedTDS)
+        print(f"RO opex : ${cost}")
+        np.testing.assert_allclose(cost,315840,rtol=0.01)
+
+    def test_YJWEC_capex(self):
+        width = 18
+        thickness = 1.8
+        height = 11
+        capex = WECecon.CAPEX(width,height,thickness)
+        print(f"WEC capex : ${capex}")
+        np.testing.assert_allclose(capex, 2719074, rtol=0.01)
+
+    def test_YJWEC_opex(self):
+        width = 18
+        thickness = 1.8
+        height = 11
+        capex = WECecon.OPEX(width,height,thickness)
+        print(f"WEC opex : ${capex}")
+        np.testing.assert_allclose(capex, 769212, rtol=0.01)
 
 if __name__ == '__main__':
     unittest.main()
