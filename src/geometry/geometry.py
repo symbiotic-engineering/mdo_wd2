7412d9836d8ed142c34bd50660238573d16861c0
degoeden <degoeden8@gmail.com>
2025-05-15 11:44:17 -0400

    IDETC edits, xdsm, and new geometry module

--- src/geometry/geometry.py
+++ src/geometry/geometry.py
@@ -3,7 +3,7 @@ import xarray as xr
 import openmdao.api as om
 from src.params import PARAMS, INPUTS
 
-class DesignVariableMapper(om.ExplicitComponent):
+class Geometry(om.ExplicitComponent):
 
     def setup(self):
         self.add_input("width", val=INPUTS["width"])
@@ -11,23 +11,19 @@ class DesignVariableMapper(om.ExplicitComponent):
         self.add_input("draft", val=PARAMS["draft"])
         self.add_input("cg_draft_factor", val=PARAMS["cg_draft_factor"])
         self.add_input("wec_mass", val=INPUTS["wec_mass"])
-        self.add_input("capacity", val=INPUTS["capacity"])
         
         self.add_output("cg", val=PARAMS["cg_draft_factor"]*PARAMS["draft"])
         self.add_output("inertia_matrix", val=PARAMS["unit_inertia"]*INPUTS["wec_mass"])
         self.add_output("Vo", val=INPUTS["width"]*INPUTS["thickness"]*PARAMS["draft"])
-        self.add_output("feedflow_cap", val=INPUTS["capacity"]/PARAMS["recovery_ratio"])
 
         self.declare_partials("cg", "draft")
         self.declare_partials("inertia_matrix", "wec_mass")
         self.declare_partials("Vo", ["width", "thickness", "draft"])
-        self.declare_partials("feedflow_cap", "capacity")
 
     def compute(self, inputs, outputs):
         outputs["cg"] = inputs["draft"]*inputs["cg_draft_factor"]
         outputs["inertia_matrix"] = inputs["wec_mass"]*PARAMS["unit_inertia"]
         outputs["Vo"] = inputs["width"]*inputs["thickness"]*inputs["draft"]
-        outputs["feedflow_cap"] = inputs["capacity"]/PARAMS["recovery_ratio"]
 
     def compute_partials(self, inputs, partials):
         partials["cg", "draft"] = inputs["cg_draft_factor"]
@@ -35,6 +31,5 @@ class DesignVariableMapper(om.ExplicitComponent):
         partials["Vo", "width"] = inputs["thickness"]*inputs["draft"]
         partials["Vo", "thickness"] = inputs["width"]*inputs["draft"]
         partials["Vo", "draft"] = inputs["width"]*inputs["thickness"]
-        partials["feedflow_cap", "capacity"] = 1/PARAMS["recovery_ratio"]
 
 
