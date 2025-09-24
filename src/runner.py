7288f2f70bc1e071ba9418cf5191a4e170c0b5dc
degoeden <degoeden8@gmail.com>
2025-09-24 16:08:47 -0400

    added ability to change seastate to runner

--- src/runner.py
+++ src/runner.py
@@ -16,13 +16,13 @@ class RunWDDS:
         self.eng = eng
         self.prob = None
 
-    def create_problem(self):
+    def create_problem(self, significant_wave_height=2.64, peak_period=9.86):
         self.prob = om.Problem(reports=None)
 
         self.prob.model.add_subsystem('Geom',geom.Geometry(),promotes_inputs=["*"],promotes_outputs=["*"])
         self.prob.model.add_subsystem('Hydro',hydro.Hydro(),promotes_inputs=["*"],promotes_outputs=["*"])
         self.prob.model.add_subsystem('DesalParams',desal.DesalParams(),promotes_inputs=["*"],promotes_outputs=["*"])
-        self.prob.model.add_subsystem('SysDyn',sysdyn.SysDyn(self.eng),promotes_inputs=["*"],promotes_outputs=["*"])
+        self.prob.model.add_subsystem('SysDyn',sysdyn.SysDyn(self.eng,significant_wave_height,peak_period),promotes_inputs=["*"],promotes_outputs=["*"])
         self.prob.model.add_subsystem('Econ',econ.Econ(),promotes_inputs=["*"],promotes_outputs=["*"])
 
         for key, val in INPUTS.items():
