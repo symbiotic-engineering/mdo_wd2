3f2eb19b3978e5bc1236ca913a43661960f4af1e
degoeden <degoeden8@gmail.com>
2025-12-18 10:59:30 -0500

    process changes

--- notebooks/nominal_model.py
+++ notebooks/nominal_model.py
@@ -4,8 +4,10 @@ parent_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_folder)
 import numpy as np
 import matlab.engine
-from src.params import PARAMS, INPUTS, OPTIMAL
+from src.params import PARAMS, INPUTS, OPTIMAL_4
 from src.runner import RunWDDS
+os.environ["TMPDIR"] = "~/scratch/matlab_tmp"
+os.makedirs("~/scratch/matlab_tmp", exist_ok=True)
 
 future_eng = matlab.engine.start_matlab(background=True)
 eng = future_eng.result()
@@ -17,7 +19,7 @@ eng.cd('..', nargout=0)
 
 Runner = RunWDDS(eng)
 Runner.create_problem()
-design = OPTIMAL
+design = OPTIMAL_4
 #design["width"] = 9.5
 #design["thickness"] = 0.95
 lcow = Runner.solve_once(design_variables=design)
